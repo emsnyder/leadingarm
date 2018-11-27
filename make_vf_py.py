48cb57a08013d642f669c5da0fb5d67c009f1342
Elaine Snyder <esnyder@stsci.edu>
2018-11-27 16:49:58 -0500

    initial commit of creation codes

---
+++ make_vf_py.py
@@ -0,0 +1,246 @@
+import numpy as np
+from astropy.io import fits
+import os
+
+datatable = '/Users/efrazer/science/datafile1418.fits'
+
+data = fits.getdata(datatable)
+
+
+def checklimits(ionflag, ionname):
+
+    if ionflag == -1:
+        flag = '# {} is a lower limit. Line is saturated.\n'.format(ionname)
+    elif ionflag == 1:
+        flag = '# {} is an upper limit. Line is blended.\n'.format(ionname)
+    elif ionflag == 0:
+        flag = None
+    else:
+        raise ValueError
+
+    return flag
+
+
+for row in data:
+
+    # if os.path.isfile('/Users/efrazer/science/scripts_new/vf_{}.py'.format(row['target'])):
+    #     continue
+
+    with open('/Users/efrazer/science/scripts_new/vf_{}.py'.format(row['target']), 'w') as f:
+
+        f.write('import numpy as np\n')
+        f.write('import VoigtFit\n')
+        f.write('import matplotlib\n')
+        f.write('matplotlib.use("GTKAgg")\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- Add the redshift of the cloud doing the absorption\n')
+        f.write('#    Estimating 0 for everything.\n')
+        f.write('z = 0.0\n')
+        f.write('\n')
+        f.write('dataset = VoigtFit.DataSet(z)\n')
+        f.write('dataset.set_name("{}")\n'.format(row['target']))
+        f.write('dataset.verbose = True\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- If log(NHI) is not known use:\n')
+        f.write('logNHI = None\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- Rebin the data set by 3 pixels\n')
+        f.write('\n')
+        f.write('def downsample_1d(myarr,factor):\n')
+        f.write('    """\n')
+        f.write('    Downsample a 1D array by averaging over *factor* pixels.\n')
+        f.write('    Crops right side if the shape is not a multiple of factor.\n')
+        f.write('\n')
+        f.write('    Got this specific function from "Adam Ginsburgs python codes" on agpy')
+        f.write('\n')
+        f.write('    myarr : numpy array\n')
+        f.write('\n')
+        f.write('    factor : how much you want to rebin the array by\n')
+        f.write('    """\n')
+        f.write('    xs = myarr.shape[0]\n')
+        f.write('    crarr = myarr[:xs-(xs % int(factor))]\n')
+        f.write('    dsarr = np.mean( np.concatenate(\n')
+        f.write('                     [[crarr[i::factor] for i in range(factor)] ]\n')
+        f.write('                     ),axis=0)\n')
+        f.write('\n')
+        f.write('    return dsarr\n')
+        f.write('\n')
+        f.write('\n')
+        
+        f.write('# -- Load the COS data (G130M and G160M if available) in ASCII format:\n')
+        
+        if os.path.isfile("/Users/efrazer/science/cosdata/{}-G160M".format(row['target'])):
+            G160M_path = "/Users/efrazer/science/cosdata/{}-G160M".format(row['target'])
+            f.write('G160M_filename = "{}"\n'.format(G160M_path))
+            f.write('\n')
+            f.write('res_g160m = 19000.\n')
+            f.write('\n')
+            f.write('wl_g160m, spec_g160m, err_g160m = np.loadtxt(G160M_filename, unpack=True)\n')
+            f.write('\n')
+            f.write('wl_g160m_rb = downsample_1d(wl_g160m, 3)\n')
+            f.write('spec_g160m_rb = downsample_1d(spec_g160m, 3)\n')
+            f.write('err_g160m_rb = downsample_1d(err_g160m, 3)\n')
+            f.write('\n')
+            f.write('dataset.add_data(wl_g160m_rb, spec_g160m_rb, '
+                    '299792.458/res_g160m, err=err_g160m_rb, normalized=False)\n')
+            f.write('\n')
+        elif os.path.isfile("/Users/efrazer/science/cosdata/{}_spec-G160M".format(row['target'])):
+            G160M_path = "/Users/efrazer/science/cosdata/{}_spec-G160M".format(row['target'])
+            f.write('G160M_filename = "{}"\n'.format(G160M_path))
+            f.write('\n')
+            f.write('res_g160m = 19000.\n')
+            f.write('\n')
+            f.write('wl_g160m, spec_g160m, err_g160m = np.loadtxt(G160M_filename, unpack=True)\n')
+            f.write('\n')
+            f.write('wl_g160m_rb = downsample_1d(wl_g160m, 3)\n')
+            f.write('spec_g160m_rb = downsample_1d(spec_g160m, 3)\n')
+            f.write('err_g160m_rb = downsample_1d(err_g160m, 3)\n')
+            f.write('\n')
+            f.write('dataset.add_data(wl_g160m_rb, spec_g160m_rb, '
+                    '299792.458/res_g160m, err=err_g160m_rb, normalized=False)\n')
+            f.write('\n')
+        else:
+            pass
+            
+        if os.path.isfile("/Users/efrazer/science/cosdata/{}-G130M".format(row['target'])):
+            G130M_path = "/Users/efrazer/science/cosdata/{}-G130M".format(row['target'])
+            f.write('G130M_filename = "{}"\n'.format(G130M_path))
+            f.write('\n')
+            f.write('res_g130m = 16000.\n')
+            f.write('\n')
+            f.write('wl_g130m, spec_g130m, err_g130m = np.loadtxt(G130M_filename, unpack=True)\n')
+            f.write('\n')
+            f.write('wl_g130m_rb = downsample_1d(wl_g130m, 3)\n')
+            f.write('spec_g130m_rb = downsample_1d(spec_g130m, 3)\n')
+            f.write('err_g130m_rb = downsample_1d(err_g130m, 3)\n')
+            f.write('\n')
+            f.write('dataset.add_data(wl_g130m_rb, spec_g130m_rb, '
+                    '299792.458/res_g130m, err=err_g130m_rb, normalized=False)\n')
+            f.write('\n')
+        elif os.path.isfile("/Users/efrazer/science/cosdata/{}_spec-G130M".format(row['target'])):
+            G130M_path = "/Users/efrazer/science/cosdata/{}_spec-G130M".format(row['target'])
+            f.write('G130M_filename = "{}"\n'.format(G130M_path))
+            f.write('\n')
+            f.write('res_g130m = 16000.\n')
+            f.write('\n')
+            f.write('wl_g130m, spec_g130m, err_g130m = np.loadtxt(G130M_filename, unpack=True)\n')
+            f.write('\n')
+            f.write('wl_g130m_rb = downsample_1d(wl_g130m, 3)\n')
+            f.write('spec_g130m_rb = downsample_1d(spec_g130m, 3)\n')
+            f.write('err_g130m_rb = downsample_1d(err_g130m, 3)\n')
+            f.write('\n')
+            f.write('dataset.add_data(wl_g130m_rb, spec_g130m_rb, '
+                    '299792.458/res_g130m, err=err_g130m_rb, normalized=False)\n')
+            f.write('\n')
+        else:
+            pass
+
+        f.write('\n')
+        f.write('# -- Change the width of velocity search region\n')
+        f.write('dataset.velspan = 1000.0\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- Add the ions we want to fit\n')
+        f.write('\n')
+        f.write('# ION WAVELENGTH  F_VALUE\n')
+        f.write('# C II    1334.5323   0.127800\n')
+        f.write('# C IV    1548.2049   1.908E-01\n')
+        f.write('# C IV    1550.7785   9.522E-02\n')
+        f.write('# Si IV   1393.7550   5.280E-01\n')
+        f.write('# Si IV   1402.7700   2.620E-01\n')
+        f.write('# Si III  1206.5000   1.660E+00\n')
+        f.write('# Si II   1260.4221   1.007E+00\n')
+        f.write('# Si II   1193.2897   4.991E-01\n')
+        f.write('# Si II   1190.4158   2.502E-01\n')
+        f.write('# O I     1302\n')
+        f.write('\n')
+
+        # don't add the lines at all if there's no detection of the ion
+        if row['logN(CII)'] != -999:
+            f.write('dataset.add_line("CII_1334")\n')
+
+        if row['logN(CIV)'] != -999:
+            f.write('dataset.add_line("CIV_1548")\n')
+            f.write('dataset.add_line("CIV_1550")\n')
+
+        if row['logN(SiII)'] != -999:
+            f.write('dataset.add_line("SiII_1260")\n')
+            f.write('dataset.add_line("SiII_1193")\n')
+            f.write('dataset.add_line("SiII_1190")\n')
+
+        if row['logN(SiIII)'] != -999:
+            f.write('dataset.add_line("SiIII_1206")\n')
+
+        if row['logN(SiIV)'] != -999:
+            f.write('dataset.add_line("SiIV_1393")\n')
+            f.write('dataset.add_line("SiIV_1402")\n')
+
+        f.write('\n')
+        f.write('\n')
+
+        # add warning to check for upper limits (blended lines) or lower limits (saturated lines)
+        f.write('# NOTES ABOUT THE DETECTIONS:\n')
+        flaglist = [checklimits(row['logN(CII)_lim'], 'log N(C II)'),
+                    checklimits(row['logN(CIV)_lim'], 'log N(C IV)'),
+                    checklimits(row['logN(SiII)_lim'], 'log N(Si II)'),
+                    checklimits(row['logN(SiIII)_lim'], 'log N(Si III)'),
+                    checklimits(row['logN(SiIV)_lim'], 'log N(Si IV)')]
+        for flag in flaglist:
+            if flag is not None:
+                f.write(flag)
+        if all(v is None for v in flaglist):
+            f.write('# All detections are OK.\n')
+
+        f.write('\n')
+        f.write('\n')
+
+        f.write('# -- Add MW components for each ion:\n')
+        
+        if row['vms'] == -999:
+            vcen = (row['vmin'] + row['vmax'])/2.
+        else:
+            vcen = row['vms']
+            
+        b_start = np.abs(row['vmax'] - row['vmin'])/2.
+        
+        f.write('#                      ion     z   b   logN\n')
+        f.write('dataset.add_component("CII",   0,  {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(b_start*2., row['logN(CII)']+1.))
+        f.write('dataset.add_component("SiII",  0., {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(b_start*2., row['logN(SiII)']+1.))
+        f.write('dataset.add_component("SiIII", 0., {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(b_start*2., row['logN(SiIII)']+1.))
+        f.write('dataset.add_component("SiIV",  0., {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(b_start*2., row['logN(SiIV)']+1.))
+        f.write('dataset.add_component("CIV",   0., {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(b_start*2., row['logN(CIV)']+1.))
+        f.write('# # add 1 to all the logN values\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- Add the MS components\n')
+        f.write('#    The z is now an offset in velocity\n')
+        f.write('dataset.add_component_velocity("CII",   {}, {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(vcen, b_start, row['logN(CII)']))
+        f.write('dataset.add_component_velocity("SiII",  {}, {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(vcen, b_start, row['logN(SiII)']))
+        f.write('dataset.add_component_velocity("SiIII", {}, {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(vcen, b_start, row['logN(SiIII)']))
+        f.write('dataset.add_component_velocity("SiIV",  {}, {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(vcen, b_start, row['logN(SiIV)']))
+        f.write('dataset.add_component_velocity("CIV",   {}, {}, {}, var_z=1, var_b=1, var_N=1)\n'.format(vcen, b_start, row['logN(CIV)']))
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- Prepare the dataset: This will prompt the user for interactive\n')
+        f.write('#    masking and normalization, as well as initiating the Parameters:\n')
+        f.write('\n')
+        f.write('dataset.cheb_order = 1\n')
+        f.write('\n')
+        f.write('dataset.prepare_dataset(norm=True, mask=True)\n')
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- Fit the dataset:\n')
+        f.write('popt, chi2 = dataset.fit()\n')
+        f.write('\n')
+        f.write('dataset.plot_fit(filename="{}.pdf", max_rows=6)\n'.format(row['target']))
+        f.write('\n')
+        f.write('\n')
+        f.write('# -- Save the dataset to file: taken from the dataset.name\n')
+        f.write('dataset.save()\n')
+        f.write('dataset.save_parameters("{}.fit")\n'.format(row['target']))
+        f.write('dataset.save_cont_parameters_to_file("{}.cont")\n'.format(row['target']))
+        f.write('dataset.save_fit_regions("{}.reg")\n'.format(row['target']))
+        f.write('\n')
---
+++ makedatatable.py
@@ -0,0 +1,114 @@
+from astropy.table import Table, Column
+from astropy import units as u
+import numpy as np
+
+datatable = Table()
+
+datatable['target'] = Column([], description='target name', dtype='S20')
+datatable['region'] = Column([], description='Region probed by target [Stream (MS), Bridge, Leading Arm (LA), LMC Halo, or CHVC]. On/Off refer to directions with/without Magellanic Hi 21cm emission', dtype='S20')
+datatable['l'] = Column([], unit='degree', description='Galatcic latitude', dtype=np.float64)
+datatable['b'] = Column([], unit='degree', description='Galactic longitude', dtype=np.float64)
+datatable['LMS'] = Column([], unit='degree', description='Magellanic latitude', dtype=np.float64)
+datatable['BMS'] = Column([], unit='degree', description='Magellanic longitude', dtype=np.float64)
+datatable['PID'] = Column([], description='HST program ID of dataset', dtype=np.int)
+datatable['vmin'] = Column([], unit='km s^-1', description='Minimum LSR velocity of Magellanic absorption', dtype=np.int)
+datatable['vmax'] = Column([], unit='km s^-1', description='Maximum LSR velocity of Magellanic absorption', dtype=np.int)
+datatable['vms'] = Column([], unit='km s^-1', description='central Magellanic LSR velocity', dtype=np.int)
+datatable['logN(HI)'] = Column([], unit='cm^-2', description='HI column density', dtype=np.float64)
+datatable['logN(HI)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['logN(SiII)'] = Column([], unit='cm^-2', description='column density of SiII', dtype=np.float64)
+datatable['logN(SiII)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['logN(SiII)_err'] = Column([], unit='cm^-2', description='error on measurement', dtype=np.float64)
+datatable['logN(SiIII)'] = Column([], unit='cm^-2', description='column density of SiIII', dtype=np.float64)
+datatable['logN(SiIII)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['logN(SiIII)_err'] = Column([], unit='cm^-2', description='error on measurement', dtype=np.float64)
+datatable['logN(SiIV)'] = Column([], unit='cm^-2', description='column density of SiIV', dtype=np.float64)
+datatable['logN(SiIV)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['logN(SiIV)_err'] = Column([], unit='cm^-2', description='error on measurement', dtype=np.float64)
+datatable['logN(CII)'] = Column([], unit='cm^-2', description='column density of CII', dtype=np.float64)
+datatable['logN(CII)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['logN(CII)_err'] = Column([], unit='cm^-2', description='error on measurement', dtype=np.float64)
+datatable['logN(CIV)'] = Column([], unit='cm^-2', description='column density of CIV', dtype=np.float64)
+datatable['logN(CIV)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['logN(CIV)_err'] = Column([], unit='cm^-2', description='error on measurement', dtype=np.float64)
+datatable['log(SiIII/SiII)'] = Column([], unit=u.dimensionless_unscaled, description='ratio of SiIII to SiII column densities', dtype=np.float64)
+datatable['log(SiIII/SiII)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['log(SiIII/SiII)_err'] = Column([], unit=u.dimensionless_unscaled, description='error on measurement', dtype=np.float64)
+datatable['log(SiIV/SiII)'] = Column([], unit=u.dimensionless_unscaled, description='ratio of SiIV to SiII column densities', dtype=np.float64)
+datatable['log(SiIV/SiII)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['log(SiIV/SiII)_err'] = Column([], unit=u.dimensionless_unscaled, description='error on measurement', dtype=np.float64)
+datatable['log(CIV/CII)'] = Column([], unit=u.dimensionless_unscaled, description='ratio of CIV to CII column densities', dtype=np.float64)
+datatable['log(CIV/CII)_lim'] = Column([], description='quantifies whether quantity is upper limit (1), lower limit (-1), or neither (0)', dtype=np.int)
+datatable['log(CIV/CII)_err'] = Column([], unit=u.dimensionless_unscaled, description='error on measurement', dtype=np.float64)
+
+
+def checklims(input_str):
+    if input_str[0] == '<':
+        lim = 1
+        val = input_str[1:]
+    elif input_str[0] == '>':
+        lim = -1
+        val = input_str[1:]
+    else:
+        lim = 0
+        val = input_str
+    return val, lim
+
+
+f = open('origdatafile.txt', 'r')
+f.readline()
+for line in f.readlines():
+    myline = line.strip()
+    sline = myline.split()
+    target = sline[0]
+    region = sline[1]
+    SiII = sline[2]
+    SiII_err = sline[3]
+    SiIII = sline[4]
+    SiIII_err = sline[5]
+    SiIV = sline[6]
+    SiIV_err = sline[7]
+    CII = sline[8]
+    CII_err = sline[9]
+    CIV = sline[10]
+    CIV_err = sline[11]
+    SiIIISiII = sline[12]
+    SiIIISiII_err = sline[13]
+    SiIVSiII = sline[14]
+    SiIVSiII_err = sline[15]
+    CIVCII = sline[16]
+    CIVCII_err = sline[17]
+    l = sline[18]
+    b = sline[19]
+    LMS = sline[20]
+    BMS = sline[21]
+    PID = sline[22]
+    vmin = sline[23]
+    vmax = sline[24]
+    vms = sline[25]
+    HI = sline[26]
+    newHI, HI_lim = checklims(HI)
+    newSiII, SiII_lim = checklims(SiII)
+    newSiIII, SiIII_lim = checklims(SiIII)
+    newSiIV, SiIV_lim = checklims(SiIV)
+    newCII, CII_lim = checklims(CII)
+    newCIV, CIV_lim = checklims(CIV)
+    newSiIIISiII, SiIIISiII_lim = checklims(SiIIISiII)
+    newSiIVSiII, SiIVSiII_lim = checklims(SiIVSiII)
+    newCIVCII, CIVCII_lim = checklims(CIVCII)
+    mylist = (target, region,float(l), float(b), float(LMS), float(BMS),
+              int(PID), int(vmin), int(vmax), int(vms),
+              float(newHI), HI_lim,
+              float(newSiII), SiII_lim, float(SiII_err),
+              float(newSiIII), SiIII_lim, float(SiIII_err),
+              float(newSiIV), SiIV_lim, float(SiIV_err),
+              float(newCII), CII_lim, float(CII_err),
+              float(newCIV), CIV_lim, float(CIV_err),
+              float(newSiIIISiII), SiIIISiII_lim, float(SiIIISiII_err),
+              float(newSiIVSiII), SiIVSiII_lim, float(SiIVSiII_err),
+              float(newCIVCII), CIVCII_lim, float(CIVCII_err)
+              )
+    datatable.add_row(mylist)
+
+# datatable.write('newdatafile.hdf5', path='data', serialize_meta=True, overwrite=True)
+datatable.write('datafile1418.fits', format='fits', overwrite=True)
