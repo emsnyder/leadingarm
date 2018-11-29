6c92c217fba488876934f2e9c426e492151d1eb4
Elaine Snyder <esnyder@stsci.edu>
2018-11-29 13:23:06 -0500

    adding OI fit

--- sightlines/NGC3125/README.md
+++ sightlines/NGC3125/README.md
@@ -2,26 +2,24 @@
 
 Found good fits for the following ions:
 
-**OI: using 2 components**
+**O I 1302: using 2 components**
 ```
 0 - MW
-1 - 
+1 - Leading Arm Component 1
+2 - Leading Arm Component 2
+Note that the errors on this fit are rather large.
 ```
-**SiII: using 2 components**
-```
-0 - MW
-1 - 
-```
-**SiIII: using 2 components**
+**Si II 1190, 1260: using 2 components**
 ```
 0 - MW
-1 - 
+1 - Leading Arm Component 1
+2 - Leading Arm Component 2
 ```
-**SiIV: using 2 components**
+**Si IV 1393, 1402: using 2 components**
 ```
 0 - MW
 1 -
 ```
 
 **Other Notes:**
- 
+ Also attempted a fit to SiIII (1206). Had to use a spline fit for the continuum. The VoigtFit code seems to not be able to plot well when a spline fit is used. Was able to fit three components with similar results as SiII.
--- sightlines/NGC3125/vf_NGC3125.py
+++ sightlines/NGC3125/vf_NGC3125.py
@@ -9,7 +9,7 @@ matplotlib.use("GTKAgg")
 z = 0.0
 
 dataset = VoigtFit.DataSet(z)
-dataset.set_name("NGC3125-SiIII")
+dataset.set_name("NGC3125-OI")
 dataset.verbose = True
 
 
@@ -39,21 +39,33 @@ def downsample_1d(myarr,factor):
 
 
 # -- Load the COS data (G130M and G160M if available) in ASCII format:
-G130M_filename = "/Users/efrazer/science/cosdata/NGC3125-G130M"
-
+# G130M_filename = "/Users/efrazer/leadingarm/sightlines/NGC3125/NGC3125-G130M"
+#
 res_g130m = 16000.
+#
+# wl_g130m, spec_g130m, err_g130m = np.loadtxt(G130M_filename, unpack=True)
+#
+# wl_g130m_rb = downsample_1d(wl_g130m, 3)
+# spec_g130m_rb = downsample_1d(spec_g130m, 3)
+# err_g130m_rb = downsample_1d(err_g130m, 3)
+#
+# dataset.add_data(wl_g130m_rb, spec_g130m_rb, 299792.458/res_g130m, err=err_g130m_rb, normalized=False)
 
-wl_g130m, spec_g130m, err_g130m = np.loadtxt(G130M_filename, unpack=True)
+G130M_filename_n = "/Users/efrazer/leadingarm/sightlines/NGC3125/NGC3125-G130M-N"
 
-wl_g130m_rb = downsample_1d(wl_g130m, 3)
-spec_g130m_rb = downsample_1d(spec_g130m, 3)
-err_g130m_rb = downsample_1d(err_g130m, 3)
+res_g130m_n = 16000.
 
-dataset.add_data(wl_g130m_rb, spec_g130m_rb, 299792.458/res_g130m, err=err_g130m_rb, normalized=False)
+wl_g130m_n, spec_g130m_n, err_g130m_n = np.loadtxt(G130M_filename_n, unpack=True)
+
+wl_g130m_rb_n = downsample_1d(wl_g130m_n, 3)
+spec_g130m_rb_n = downsample_1d(spec_g130m_n, 3)
+err_g130m_rb_n = downsample_1d(err_g130m_n, 3)
+
+dataset.add_data(wl_g130m_rb_n, spec_g130m_rb_n, 299792.458/res_g130m, err=err_g130m_rb_n, normalized=False)
 
 
 # -- Change the width of velocity search region
-dataset.velspan = 800.0
+dataset.velspan = 1200.0
 
 
 # -- Add the ions we want to fit
@@ -70,13 +82,13 @@ dataset.velspan = 800.0
 # SiII    1190.4158   2.502E-01
 
 # dataset.add_line("CII_1334")
-#dataset.add_line("SiII_1260")
+# dataset.add_line("SiII_1260")
 # dataset.add_line("SiII_1193")  # blended
 # dataset.add_line("SiII_1190")  # blended
-dataset.add_line("SiIII_1206")
+# dataset.add_line("SiIII_1206")
 # dataset.add_line("SiIV_1393")
 # dataset.add_line("SiIV_1402")
-
+dataset.add_line("OI_1302")
 
 # NOTES ABOUT THE DETECTIONS:
 # log N(C II) is an upper limit. Line is blended.
@@ -92,30 +104,36 @@ dataset.add_line("SiIII_1206")
 # dataset.add_component_velocity("SiII",  190, 30.0, 13.1, var_z=1, var_b=1, var_N=1)
 
 # SiIII
-dataset.add_component("SiIII",  0., 60.0, 13.8, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("SiIII",  120, 20.0, 13.1, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("SiIII",  190, 30.0, 13.1, var_z=1, var_b=1, var_N=1)
+# dataset.add_component("SiIII",  0., 60.0, 13.8, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiIII",  120, 20.0, 13.1, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiIII",  190, 30.0, 13.1, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("SiIII",  210, 30.0, 13.1, var_z=1, var_b=1, var_N=1)
+
+
+# OI
+dataset.add_component("OI",  0., 40.0, 14.8, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("OI",  110, 40.0, 14.1, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("OI",  200, 30.0, 14.1, var_z=1, var_b=1, var_N=1)
 
 
 # -- Prepare the dataset: This will prompt the user for interactive
 #    masking and normalization, as well as initiating the Parameters:
 
-# dataset.cheb_order = 1
-dataset.cheb_order = -1
-dataset.norm_method = 'spline'
+dataset.cheb_order = 1
+# dataset.cheb_order = -1
+# dataset.norm_method = 'spline'
 dataset.prepare_dataset(norm=True, mask=True)
 
 
 # -- Fit the dataset:
 popt, chi2 = dataset.fit()
 
-dataset.plot_fit(filename="NGC3125-SiIII.pdf", max_rows=6)
+dataset.plot_fit(filename="NGC3125-OI.pdf", max_rows=6)
 
 
 # -- Save the dataset to file: taken from the dataset.name
 dataset.save()
-dataset.save_parameters("NGC3125-SiIII.fit")
-dataset.save_cont_parameters_to_file("NGC3125-SiIII.cont")
-dataset.save_fit_regions("NGC3125-SiIII.reg")
+dataset.save_parameters("NGC3125-OI.fit")
+dataset.save_cont_parameters_to_file("NGC3125-OI.cont")
+dataset.save_fit_regions("NGC3125-OI.reg")
 
