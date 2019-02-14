637a183c45d7271bb74c802967ff11bdf64efb76
Elaine Snyder <esnyder@stsci.edu>
2019-02-14 16:55:51 -0500

    adding HE0226

--- sightlines/HE0226-4110/vf_HE0226-4110.py
+++ sightlines/HE0226-4110/vf_HE0226-4110.py
@@ -9,7 +9,7 @@ matplotlib.use("GTKAgg")
 z = 0.0
 
 dataset = VoigtFit.DataSet(z)
-dataset.set_name("HE0226-4110-XXX")
+dataset.set_name("HE0226-4110-SiIII")
 dataset.verbose = True
 
 
@@ -63,7 +63,7 @@ dataset.add_data(wl_g130m_rb, spec_g130m_rb, 299792.458/res_g130m, err=err_g130m
 
 
 # -- Change the width of velocity search region
-dataset.velspan = 1000.0
+dataset.velspan = 600.0
 
 
 # -- Add the ions we want to fit
@@ -83,7 +83,7 @@ dataset.velspan = 1000.0
 # dataset.add_line("SiII_1260")
 # dataset.add_line("SiII_1193")
 # dataset.add_line("SiII_1190")
-# dataset.add_line("SiIII_1206")
+dataset.add_line("SiIII_1206")
 # dataset.add_line("SiIV_1393")
 # dataset.add_line("SiIV_1402")
 # dataset.add_line("CII_1334")
@@ -101,25 +101,36 @@ dataset.velspan = 1000.0
 #    ordered by [ion, z, b, logN] then switches to fix z, b, or N during the fit
 
 # SiII
-# dataset.add_component("SiII",  0., 155.0, 14.12, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("SiII",  195, 77.5, 13.12, var_z=1, var_b=1, var_N=1)
+# dataset.add_component("SiII",  0., 30.0, 14.12, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiII",  110, 30.5, 13.12, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiII",  155, 30.5, 13.12, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiII",  185, 30.5, 13.12, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiII",  205, 30.5, 13.12, var_z=1, var_b=1, var_N=1)
 
 # SiIII
-# dataset.add_component("SiIII", 0., 155.0, 14.26, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("SiIII", 195, 77.5, 13.26, var_z=1, var_b=1, var_N=1)
+dataset.add_component("SiIII", 0., 35.0, 13.5, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("SiIII", 95, 17.5, 13.26, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("SiIII", 165, 27.5, 13.26, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("SiIII", 215, 27.5, 13.26, var_z=1, var_b=1, var_N=1)
 
 # SiIV
 # dataset.add_component("SiIV",  0., 155.0, 14.03, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("SiIV",  195, 77.5, 13.03, var_z=1, var_b=1, var_N=1)
 
 # CII
-# dataset.add_component("CII",   0,  155.0, 15.28, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("CII",   195, 77.5, 14.28, var_z=1, var_b=1, var_N=1)
+# dataset.add_component("CII",   0,  30, 14.4, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CII",   80, 10.0, 13.28, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CII",   109, 10.0, 13.28, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CII",   161, 10.5, 13.66, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CII",   207, 25.5, 13.99, var_z=1, var_b=1, var_N=1)
+
 
 # CIV
-# dataset.add_component("CIV",   0., 155.0, 14.88, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("CIV",   195, 77.5, 13.88, var_z=1, var_b=1, var_N=1)
-
+# dataset.add_component("CIV",   0., 100.0, 14.3, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   80, 77.5, 13.88, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   130, 10.0, 13.28, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   175, 10.5, 13.66, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   215, 25.5, 13.99, var_z=1, var_b=1, var_N=1)
 
 # -- Prepare the dataset: This will prompt the user for interactive
 #    masking and normalization, as well as initiating the Parameters:
@@ -132,12 +143,12 @@ dataset.prepare_dataset(norm=True, mask=True)
 # -- Fit the dataset:
 popt, chi2 = dataset.fit()
 
-dataset.plot_fit(filename="HE0226-4110-XXX.pdf", max_rows=6)
+dataset.plot_fit(filename="HE0226-4110-SiIII.pdf")
 
 
 # -- Save the dataset to file: taken from the dataset.name
 dataset.save()
-dataset.save_parameters("HE0226-4110-XXX.fit")
-dataset.save_cont_parameters_to_file("HE0226-4110-XXX.cont")
-dataset.save_fit_regions("HE0226-4110-XXX.reg")
+dataset.save_parameters("HE0226-4110-SiIII.fit")
+dataset.save_cont_parameters_to_file("HE0226-4110-SiIII.cont")
+dataset.save_fit_regions("HE0226-4110-SiIII.reg")
 
