68a91e8a1fd8cd4e5cdba2ef635e112a8322e5c0
Elaine Snyder <esnyder@stsci.edu>
2019-02-15 10:26:38 -0500

    adding MRC2251

--- sightlines/MRC2251-178/README.md
+++ sightlines/MRC2251-178/README.md
@@ -1,12 +1,22 @@
-## MRC2251-178 - Not yet complete
+## MRC2251-178 
 **Found good fits for the following ions:**
 
-Ion using N components:
+C II 1334 using 2 components:
 ```
 0 - MW
-1 - Stream
+1 - Stream 1
 ```
 
+C IV 1548, 1550 using 2 components:
+```
+0 - MW
+1 - Stream 1
+```
 
+Si III 1206 using 2 components:
+```
+0 - MW
+1 - Stream 1
+```
 **Other Notes:**
 
--- sightlines/MRC2251-178/vf_MRC2251-178.py
+++ sightlines/MRC2251-178/vf_MRC2251-178.py
@@ -9,7 +9,7 @@ matplotlib.use("GTKAgg")
 z = 0.0
 
 dataset = VoigtFit.DataSet(z)
-dataset.set_name("MRC2251-178-XXX")
+dataset.set_name("MRC2251-178-SiIII")
 dataset.verbose = True
 
 
@@ -83,7 +83,7 @@ dataset.velspan = 1000.0
 # dataset.add_line("SiII_1260")
 # dataset.add_line("SiII_1193")
 # dataset.add_line("SiII_1190")
-# dataset.add_line("SiIII_1206")
+dataset.add_line("SiIII_1206")
 # dataset.add_line("SiIV_1393")
 # dataset.add_line("SiIV_1402")
 # dataset.add_line("CII_1334")
@@ -105,8 +105,8 @@ dataset.velspan = 1000.0
 # dataset.add_component_velocity("SiII",  -260, 75.0, 12.96, var_z=1, var_b=1, var_N=1)
 
 # SiIII
-# dataset.add_component("SiIII", 0., 150.0, 13.99, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("SiIII", -260, 75.0, 12.99, var_z=1, var_b=1, var_N=1)
+dataset.add_component("SiIII", 0., 150.0, 13.99, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("SiIII", -260, 75.0, 12.99, var_z=1, var_b=1, var_N=1)
 
 # SiIV
 # dataset.add_component("SiIV",  0., 150.0, 14.04, var_z=1, var_b=1, var_N=1)
@@ -132,12 +132,12 @@ dataset.prepare_dataset(norm=True, mask=True)
 # -- Fit the dataset:
 popt, chi2 = dataset.fit()
 
-dataset.plot_fit(filename="MRC2251-178-XXX.pdf", max_rows=6)
+dataset.plot_fit(filename="MRC2251-178-SiIII.pdf", max_rows=6)
 
 
 # -- Save the dataset to file: taken from the dataset.name
 dataset.save()
-dataset.save_parameters("MRC2251-178-XXX.fit")
-dataset.save_cont_parameters_to_file("MRC2251-178-XXX.cont")
-dataset.save_fit_regions("MRC2251-178-XXX.reg")
+dataset.save_parameters("MRC2251-178-SiIII.fit")
+dataset.save_cont_parameters_to_file("MRC2251-178-SiIII.cont")
+dataset.save_fit_regions("MRC2251-178-SiIII.reg")
 
