7abb609904097595b9573331e9148967bf87a9b6
Elaine Snyder <esnyder@stsci.edu>
2019-10-04 13:22:09 -0400

    Fix for plotting script. Closes #1

--- plotallions.py
+++ plotallions.py
@@ -63,53 +63,63 @@ def wl_to_v(input_dict, ionval):
     if '1190' in ionval:
         restwl = 1190.4158
         col = 'orange'
-        lab = 'Si II'
+        lab = 'Si II ' + r'$\lambda$' + '1190'
         key = 'SiII'
+        lev = 0
     elif '1193' in ionval:
         restwl = 1193.2897
         col = 'orange'
-        lab = 'Si II'
+        lab = 'Si II ' + r'$\lambda$' + '1193'
         key = 'SiII'
+        lev = 0
     elif '1260' in ionval:
         restwl = 1260.4221
         col = 'orange'
-        lab = 'Si II'
+        lab = 'Si II ' + r'$\lambda$' + '1260'
         key = 'SiII'
+        lev = 0
     elif '1253' in ionval:
         restwl = 1253.8110
         col = 'orange'
-        lab = 'Si II'
+        lab = 'Si II ' + r'$\lambda$' + '1253'
         key = 'SiII'
+        lev = 0
     elif '1206' in ionval:
         restwl = 1206.5000
         col = 'deeppink'
-        lab = 'Si III'
+        lab = 'Si III ' + r'$\lambda$' + '1206'
         key = 'SiIII'
+        lev = 1
     elif '1393' in ionval:
         restwl = 1393.7550
         col = 'deepskyblue'
-        lab = 'Si IV'
+        lab = 'Si IV ' + r'$\lambda$' + '1393'
         key = 'SiIV'
+        lev = 2
     elif '1402' in ionval:
         restwl = 1402.7700
         col = 'deepskyblue'
-        lab = 'Si IV'
+        lab = 'Si IV ' + r'$\lambda$' + '1402'
         key = 'SiIV'
+        lev = 2
     elif '1334' in ionval:
         restwl = 1334.5323
         col = 'orange'
-        lab = 'C II'
+        lab = 'C II ' + r'$\lambda$' + '1334'
         key = 'CII'
+        lev = 0
     elif '1548' in ionval:
         restwl = 1548.2049
         col = 'deepskyblue'
-        lab = 'C IV'
+        lab = 'C IV ' + r'$\lambda$' + '1548'
         key = 'CIV'
+        lev = 2
     elif '1550' in ionval:
         restwl = 1550.7785
         col = 'deepskyblue'
-        lab = 'C IV'
+        lab = 'C IV ' + r'$\lambda$' + '1550'
         key = 'CIV'
+        lev = 2
     else:
         raise ValueError('cant find input ion: {}'.format(ionval))
 
@@ -117,7 +127,7 @@ def wl_to_v(input_dict, ionval):
 
     v = ((wavearr - restwl)/restwl) * C
 
-    return v, col, lab, key
+    return v, col, lab, key, lev
 
 
 # -------------------------------------------------------------
@@ -134,66 +144,43 @@ if __name__ == '__main__':
         ionlist = IONSTOUSE[sightline]
 
         if len(ionlist) > 1:
-            fig, axes = plt.subplots(len(ionlist), 1, sharex=True, figsize=(7, 3*len(ionlist)))
-            fig.subplots_adjust(hspace=0)
-            axes[0].set_title(sightline, fontsize=20)
-            axes[0].set_xlim(-600, 600)
 
-            for n, ion in enumerate(ionlist):
+            fig, axes = plt.subplots(3, 1, sharex=True, sharey=True, figsize=(7, 9))
 
-                vel_arr, colorval, labelval, keyval = wl_to_v(fitdict[sightline], ion)
+            for ion in ionlist:
 
-                axes[n].plot(vel_arr, fitdict[sightline][keyval]['flux'],
-                             color=colorval, linestyle='-', label=labelval)
-                axes[n].plot(vel_arr, fitdict[sightline][keyval]['best fit line'],
-                             color='black', linestyle='-', label='Best Fit')
+                vel_arr, colorval, labelval, keyval, level = wl_to_v(fitdict[sightline], ion)
+
+                axes[level].plot(vel_arr, fitdict[sightline][keyval]['flux'],
+                                 color=colorval, linestyle='-', label=labelval)
+                axes[level].plot(vel_arr, fitdict[sightline][keyval]['best fit line'],
+                                 color='black', linestyle='-', label='Best Fit')
+
                 for zcomp in fitdict[sightline][keyval]['components']['redshift']:
                     vcomp = zcomp*C
-                    axes[n].axvline(vcomp, color=colorval, linestyle='--')
-                axes[n].legend(prop={'size': 12})
-                axes[n].set_yticks(np.arange(0, 1.2, 0.2))
-                # axes[n].set_ylim(0, 1.2)
+                    axes[level].axvline(vcomp, color=colorval, linestyle='--')
+
+                axes[level].legend(prop={'size': 12})
+                direction = fitdict[sightline][keyval]['direction'][0].split('-')[0]
+
+            for n in np.arange(0, 3):
+                axes[n].set_ylim(-0.1, 1.5)
+                axes[n].set_yticks(np.arange(0, 1.6, 0.2))
+                axes[n].set_xticks(np.arange(-600, 700, 100))
                 axes[n].set_axisbelow(True)
                 axes[n].yaxis.grid(color='lightgray', linestyle='dashed')
                 axes[n].xaxis.grid(color='lightgray', linestyle='dashed')
                 plt.setp(axes[n].get_xticklabels(), fontsize=12)
                 plt.setp(axes[n].get_yticklabels(), fontsize=12)
 
+            fig.subplots_adjust(hspace=0)
+            axes[0].set_title('{}   ({})'.format(sightline, direction), fontsize=20)
+            axes[0].set_xlim(-600, 600)
             fig.add_subplot(111, frameon=False)
             plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-            plt.xlabel("velocity", fontsize=16)
+            plt.xlabel('velocity (' + r'$km\>s^{-1}$' + ')', fontsize=16)
             plt.ylabel("normalized flux", fontsize=16)
 
-            plt.show()
-
-        elif len(ionlist) == 1:
-            fig, axes = plt.subplots(len(ionlist), 1, sharex=True, figsize=(7, 3))
-            fig.subplots_adjust(hspace=0.1)
-
-            for n, ion in enumerate(ionlist):
-
-                vel_arr, colorval, labelval, keyval = wl_to_v(fitdict[sightline], ion)
-
-                axes.plot(vel_arr, fitdict[sightline][keyval]['flux'],
-                          color=colorval, linestyle='-', label=labelval)
-                axes.plot(vel_arr, fitdict[sightline][keyval]['best fit line'],
-                          color='black', linestyle='-', label='Best Fit')
-                for zcomp in fitdict[sightline][keyval]['components']['redshift']:
-                    vcomp = zcomp*C
-                    axes.axvline(vcomp, color=colorval, linestyle='--')
-                axes.legend(prop={'size': 12})
-                axes.set_axisbelow(True)
-                axes.set_yticks(np.arange(0, 1.2, 0.2))
-                axes.yaxis.grid(color='lightgray', linestyle='dashed')
-                axes.xaxis.grid(color='lightgray', linestyle='dashed')
-
-            axes.set_title(sightline)
-            axes.set_xlim(-600, 600)
-            fig.add_subplot(111, frameon=False)
-            plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-            plt.xlabel("velocity")
-            plt.ylabel("normalized flux")
-            plt.tight_layout()
             plt.show()
 
         else:
