830cbeaf0038ca74edb033c429b49b0026213146
Elaine Snyder <esnyder@stsci.edu>
2019-12-06 10:21:25 -0500

    updating LBQS0107-0235

--- sightlines/LBQS0107-0235/README.md
+++ sightlines/LBQS0107-0235/README.md
@@ -6,18 +6,17 @@ C II 1334 using 4 components:
 0 - MW
 1 - Stream 1
 2 - Stream 2
-3 - Stream 3 (questionable - very large error)
 ```
 
 Si III 1206 using 5 components:
 ```
 0 - MW
-1 - Not sure if Stream or MW
+1 - Not sure if Stream or MW - taken out
 2 - Stream
 3 - Stream
 4 - Stream (very large error)
 ```
-C IV 1548, 1550 using 6 components:
+C IV 1548, 1550 using 3 components:
 ```
 0 - MW 1
 1 - MW 2
@@ -26,4 +25,4 @@ C IV 1548, 1550 using 6 components:
 
 
 **Other Notes:**
-
+removed all CIV components due to low S/N
--- sightlines/LBQS0107-0235/vf_LBQS0107-0235.py
+++ sightlines/LBQS0107-0235/vf_LBQS0107-0235.py
@@ -9,7 +9,7 @@ matplotlib.use("GTKAgg")
 z = 0.0
 
 dataset = VoigtFit.DataSet(z)
-dataset.set_name("LBQS0107-0235-CIV")
+dataset.set_name("LBQS0107-0235-CII")
 dataset.verbose = True
 
 
@@ -86,9 +86,9 @@ dataset.velspan = 1200.0
 # dataset.add_line("SiIII_1206")
 # dataset.add_line("SiIV_1393")
 # dataset.add_line("SiIV_1402")
-# dataset.add_line("CII_1334")
-dataset.add_line("CIV_1548")
-dataset.add_line("CIV_1550")
+dataset.add_line("CII_1334")
+# dataset.add_line("CIV_1548")
+# dataset.add_line("CIV_1550")
 
 
 # NOTES ABOUT THE DETECTIONS:
@@ -116,19 +116,19 @@ dataset.add_line("CIV_1550")
 # dataset.add_component_velocity("SiIV",  -252, 95.0, 13.14, var_z=1, var_b=1, var_N=1)
 
 # CII
-# dataset.add_component("CII",   0,  50.0, 14.67, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("CII",   -190, 20.0, 13.67, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("CII",   -262, 10.0, 13.1, var_z=1, var_b=1, var_N=1)
+dataset.add_component("CII",   0,  50.0, 14.67, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("CII",   -190, 20.0, 13.67, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("CII",   -262, 10.0, 13.1, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("CII",   -305, 10.0, 13.1, var_z=1, var_b=1, var_N=1)
 
 
 # CIV
-dataset.add_component("CIV",   0., 40.0, 13.7, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("CIV",   -70, 40.0, 13.7, var_z=1, var_b=1, var_N=1)
+# dataset.add_component("CIV",   0., 40.0, 13.7, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   -70, 40.0, 13.7, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("CIV",   -109, 10.0, 12.67, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("CIV",   -190, 10.0, 12.67, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("CIV",   -242, 10.0, 12.8, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("CIV",   -280, 10.0, 13.1, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   -280, 10.0, 13.1, var_z=1, var_b=1, var_N=1)
 
 
 # -- Prepare the dataset: This will prompt the user for interactive
@@ -142,12 +142,11 @@ dataset.prepare_dataset(norm=True, mask=True)
 # -- Fit the dataset:
 popt, chi2 = dataset.fit()
 
-dataset.plot_fit(filename="LBQS0107-0235-CIV.pdf")
+dataset.plot_fit(filename="LBQS0107-0235-CII.pdf")
 
 
 # -- Save the dataset to file: taken from the dataset.name
 dataset.save()
-dataset.save_parameters("LBQS0107-0235-CIV.fit")
-dataset.save_cont_parameters_to_file("LBQS0107-0235-CIV.cont")
-dataset.save_fit_regions("LBQS0107-0235-CIV.reg")
-
+dataset.save_parameters("LBQS0107-0235-CII.fit")
+dataset.save_cont_parameters_to_file("LBQS0107-0235-CII.cont")
+dataset.save_fit_regions("LBQS0107-0235-CII.reg")
