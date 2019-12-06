376fb97c1169554df929e44a1762d59b8061a699
Elaine Snyder <esnyder@stsci.edu>
2019-12-06 10:59:55 -0500

    updating MRK1044

--- sightlines/MRK1044/vf_MRK1044.py
+++ sightlines/MRK1044/vf_MRK1044.py
@@ -9,7 +9,7 @@ matplotlib.use("GTKAgg")
 z = 0.0
 
 dataset = VoigtFit.DataSet(z)
-dataset.set_name("MRK1044-CIV")
+dataset.set_name("MRK1044-CII")
 dataset.verbose = True
 
 
@@ -63,7 +63,7 @@ dataset.add_data(wl_g130m_rb, spec_g130m_rb, 299792.458/res_g130m, err=err_g130m
 
 
 # -- Change the width of velocity search region
-dataset.velspan = 1000.0
+dataset.velspan = 800.0
 
 
 # -- Add the ions we want to fit
@@ -86,9 +86,9 @@ dataset.velspan = 1000.0
 # dataset.add_line("SiIII_1206")
 # dataset.add_line("SiIV_1393")
 # dataset.add_line("SiIV_1402")
-# dataset.add_line("CII_1334")
-dataset.add_line("CIV_1548")
-dataset.add_line("CIV_1550")
+dataset.add_line("CII_1334")
+# dataset.add_line("CIV_1548")
+# dataset.add_line("CIV_1550")
 
 
 # NOTES ABOUT THE DETECTIONS:
@@ -117,22 +117,23 @@ dataset.add_line("CIV_1550")
 # dataset.add_component_velocity("SiIV",  -220, 75.0, 12.97, var_z=1, var_b=1, var_N=1)
 
 # CII
-# dataset.add_component("CII",   0,  50.0, 14.0, var_z=1, var_b=1, var_N=1)
+dataset.add_component("CII",   0,  50.0, 15.0, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("CII",   -35, 30.0, 13.8, var_z=1, var_b=1, var_N=1)
 # dataset.add_component_velocity("CII",   -170, 10.0, 13.0, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("CII",   -220, 30.0, 13.58, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("CII",   -210, 30.0, 13.58, var_z=1, var_b=1, var_N=1)
 
 # CIV
-dataset.add_component("CIV",   0., 50.0, 13.8, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("CIV",   -50, 20.0, 13.0, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("CIV",   -120, 10.0, 13.0, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("CIV",   -220, 10.0, 12.7, var_z=1, var_b=1, var_N=1)
+# dataset.add_component("CIV",   0., 50.0, 13.8, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   -50, 20.0, 13.0, var_z=1, var_b=1, var_N=1)
+## dataset.add_component_velocity("CIV",   -120, 10.0, 13.0, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("CIV",   -220, 10.0, 12.7, var_z=1, var_b=1, var_N=1)
 
 
 # -- Prepare the dataset: This will prompt the user for interactive
 #    masking and normalization, as well as initiating the Parameters:
 
-dataset.cheb_order = 1
+dataset.cheb_order = 0
+dataset.norm_method = 'spline'
 
 dataset.prepare_dataset(norm=True, mask=True)
 
@@ -140,12 +141,12 @@ dataset.prepare_dataset(norm=True, mask=True)
 # -- Fit the dataset:
 popt, chi2 = dataset.fit()
 
-dataset.plot_fit(filename="MRK1044-CIV.pdf")
+# dataset.plot_fit(filename="MRK1044-CII.pdf")
 
 
 # -- Save the dataset to file: taken from the dataset.name
 dataset.save()
-dataset.save_parameters("MRK1044-CIV.fit")
-dataset.save_cont_parameters_to_file("MRK1044-CIV.cont")
-dataset.save_fit_regions("MRK1044-CIV.reg")
+dataset.save_parameters("MRK1044-CII.fit")
+dataset.save_cont_parameters_to_file("MRK1044-CII.cont")
+dataset.save_fit_regions("MRK1044-CII.reg")
 
