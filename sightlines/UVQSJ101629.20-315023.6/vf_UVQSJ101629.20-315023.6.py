3fc0f5813d8fdd1c128ef2c4466001f5a4432cdf
Elaine Snyder <esnyder@stsci.edu>
2020-01-16 15:35:23 -0500

    updating UVQS...

--- sightlines/UVQSJ101629.20-315023.6/vf_UVQSJ101629.20-315023.6.py
+++ sightlines/UVQSJ101629.20-315023.6/vf_UVQSJ101629.20-315023.6.py
@@ -9,7 +9,7 @@ matplotlib.use("GTKAgg")
 z = 0.0
 
 dataset = VoigtFit.DataSet(z)
-dataset.set_name("UVQSJ101629.20-315023.6-OI")
+dataset.set_name("UVQSJ101629.20-315023.6-SiIII")
 dataset.verbose = True
 
 
@@ -62,7 +62,7 @@ dataset.add_data(wl_g130m_rb_n, spec_g130m_rb_n, 299792.458/res_g130m, err=err_g
 
 
 # -- Change the width of velocity search region
-dataset.velspan = 1000.0
+dataset.velspan = 800.0
 
 
 # -- Add the ions we want to fit
@@ -82,13 +82,13 @@ dataset.velspan = 1000.0
 # dataset.add_line("SiII_1260")
 # dataset.add_line("SiII_1193")
 # dataset.add_line("SiII_1190")
-# dataset.add_line("SiIII_1206")
+dataset.add_line("SiIII_1206")
 # dataset.add_line("SiIV_1393")
 # dataset.add_line("SiIV_1402")
 # dataset.add_line("CII_1334")
 # dataset.add_line("CIV_1548")
 # dataset.add_line("CIV_1550")
-dataset.add_line("OI_1302")
+# dataset.add_line("OI_1302")
 
 
 # NOTES ABOUT THE DETECTIONS:
@@ -110,23 +110,24 @@ dataset.add_line("OI_1302")
 # dataset.add_component_velocity("SiII",  250, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
 
 # SiIII
-# dataset.add_component("SiIII",  0., 30.0, 13.5, var_z=1, var_b=1, var_N=1)
-# # dataset.add_component_velocity("SiIII",  50, 30.0, 13.5, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("SiIII",  160, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
-# # dataset.add_component_velocity("SiIII",  130, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
-# # dataset.add_component_velocity("SiIII",  200, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
-# # dataset.add_component_velocity("SiIII",  230, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
-# dataset.add_component_velocity("SiIII",  240, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
+dataset.add_component("SiIII",  0., 30.0, 13.5, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiIII",  50, 30.0, 13.5, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("SiIII",  160, 10.0, 12.5, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiIII",  130, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiIII",  200, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("SiIII",  230, 20.0, 12.5, var_z=1, var_b=1, var_N=1)
+dataset.add_component_velocity("SiIII",  230, 15.0, 12.5, var_z=1, var_b=1, var_N=1)
 
 # OI
-dataset.add_component("OI",  0., 50.0, 15.3, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("OI",  150, 15.0, 14.05, var_z=1, var_b=1, var_N=1)
-dataset.add_component_velocity("OI",  200, 15.0, 14.05, var_z=1, var_b=1, var_N=1)
+# dataset.add_component("OI",  0., 50.0, 15.3, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("OI",  150, 15.0, 14.05, var_z=1, var_b=1, var_N=1)
+# dataset.add_component_velocity("OI",  200, 15.0, 14.05, var_z=1, var_b=1, var_N=1)
 
 
 # -- Prepare the dataset: This will prompt the user for interactive
 #    masking and normalization, as well as initiating the Parameters:
 
+dataset.norm_method = 'spline'
 dataset.cheb_order = 1
 
 dataset.prepare_dataset(norm=True, mask=True)
@@ -135,11 +136,11 @@ dataset.prepare_dataset(norm=True, mask=True)
 # -- Fit the dataset:
 popt, chi2 = dataset.fit()
 
-dataset.plot_fit(filename="UVQSJ101629.20-315023.6-OI.pdf")
+# dataset.plot_fit(filename="UVQSJ101629.20-315023.6-SiIII.pdf")
 
 
 # -- Save the dataset to file: taken from the dataset.name
 dataset.save()
-dataset.save_parameters("UVQSJ101629.20-315023.6-OI.fit")
-dataset.save_cont_parameters_to_file("UVQSJ101629.20-315023.6-OI.cont")
-dataset.save_fit_regions("UVQSJ101629.20-315023.6-OI.reg")
+dataset.save_parameters("UVQSJ101629.20-315023.6-SiIII.fit")
+dataset.save_cont_parameters_to_file("UVQSJ101629.20-315023.6-SiIII.cont")
+dataset.save_fit_regions("UVQSJ101629.20-315023.6-SiIII.reg")
